2b4dbd718e0fb55aae2727323feefeb6dcf1763d
Max Ziebell <mail@maxziebell.de>
2021-01-21 23:35:41 +0100

    v1.0.3

--- HypePowerPack.hype-export.py
+++ HypePowerPack.hype-export.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 # 	HypePowerPack.hype-export.py
-#	Just some helpful additional actions for ...
+#	Just some helpful additional actions for Tumult Hype
 #
 #	v1.0.0 Logic, Queries, Expressions and Variables
 #	v1.0.1 Fixed scope, HypeDocumentLoad in functions()
 #	v1.0.2 limited to id, refactored python, IIFE
+#	v1.0.3 refactored JS, streamline API, replace Eval with new Function
 #
 #
 #	MIT License
@@ -19,66 +20,64 @@ import os
 
 # functions for conditions to inject in generated script
 javascript_for_actions = """
+/** 
+* Hype Power Pack v1.0.3 by Max Ziebell
+*/
+
 (function () {
 	/* @const */
 	const _standalone = false;
 
 	if("HYPE_eventListeners" in window === false) window.HYPE_eventListeners = Array();
-	window.HYPE_eventListeners.push({"type":"HypeDocumentLoad", "callback":function (hypeDocument, element, event) {
+	window.HYPE_eventListeners.push({"type":"HypeDocumentLoad", "callback":function (hypeDocument, _element, _event) {
 		
 		if (!_standalone) if (hypeDocument.documentName()!=="${hype_id}") return;
 
-		hypeDocument.Conditional_Behavior = function (expression, isTrueBehavior, isFalseBehavior) {
-			try {
-				for(var key in hypeDocument.customData) eval('var '+ key +' = hypeDocument.customData[key];');
-				var result = eval(expression);
-			} catch (e){
-				alert ("Condition Error: "+e);
-				return;
-			}
-			if (result && isTrueBehavior){
-				hypeDocument.triggerCustomBehaviorNamed(isTrueBehavior);
-			} else if (!result && isFalseBehavior){
-				hypeDocument.triggerCustomBehaviorNamed(isFalseBehavior);
+		var validNames = new RegExp('^[a-zA-Z_$][0-9a-zA-Z_$]*$');
+
+		hypeDocument.conditionalBehavior = function (expression, isTrueBehavior, isFalseBehavior) {
+			if (!expression || (!isTrueBehavior && !isFalseBehavior)) return;
+			var result = this.runJavaScriptExpression(expression, 'Condition Error');
+			if (result) {
+				if (isTrueBehavior) hypeDocument.triggerCustomBehaviorNamed(isTrueBehavior);
+			} else {
+				if (isFalseBehavior) hypeDocument.triggerCustomBehaviorNamed(isFalseBehavior);
 			}
 		}
 
-		hypeDocument.Set_Variable = function (name, expression) {
-			if (!hypeDocument.customData[name]) hypeDocument.customData[name] = null;
-			try {
-				for(var key in hypeDocument.customData) eval('var '+ key +' = hypeDocument.customData[key];');
-				eval('hypeDocument.customData[name]='+expression);
-			} catch (e){
-				alert ("Variable Error: "+e);
-			}
+		hypeDocument.setVariable = function (variable, expression) {
+			if (!variable || !expression) return;
+			if (!validNames.test(variable)) return;
+			if (!hypeDocument.customData[variable]) hypeDocument.customData[variable] = null;
+			hypeDocument.customData[variable] = this.runJavaScriptExpression(expression, 'Variable Error');
 		}
 
-		hypeDocument.Run_Function_by_Selector = function (fnc, selector) {
-			if (!hypeDocument.functions()[fnc]) return;
-			if (!selector) return;
-			try {
-				var sceneElm = document.getElementById(hypeDocument.currentSceneId());
-				var elms = sceneElm.querySelectorAll(selector);
-				elms.forEach(function(elm){
-					hypeDocument.functions()[fnc].call(window, hypeDocument, elm, {type:'HypeQuery'});
-				});
-			} catch (e){
-				alert ("Run Query Error: "+e);
-			}
+		hypeDocument.runFunctionBySelector = function (fnc, selector) {
+			if (!hypeDocument.functions()[fnc] || !selector) return;
+			var sceneElm = document.getElementById(hypeDocument.currentSceneId());
+			var elms = sceneElm.querySelectorAll(selector);
+			elms.forEach(function(elm){
+				hypeDocument.functions()[fnc].call(window, hypeDocument, elm, {type:'runFunctionBySelector'});
+			});
 		}
 
-		hypeDocument.Run_JavaScript_Expression = function (expression) {
+		hypeDocument.runJavaScriptExpression = function (expression, msg, omitContext, omitError) {
+			if (!expression) return;
+ 			var context='';
+			if (!omitContext) for(var variable in hypeDocument.customData) {
+				if (validNames.test(variable)) 
+					context+='var '+ variable +' = hypeDocument.customData["'+ variable +'"];';
+			}
 			try {
-				for(var key in hypeDocument.customData) eval('var '+ key +' = hypeDocument.customData[key];');
-				eval(expression);
+				return Function('hypeDocument', '"use strict";'+context+'return (' + expression + ')')(hypeDocument);
 			} catch (e){
-				alert ("Expression Error: "+e);
+				alert ((msg||'Expression Error')+(!omitError? ': '+e:''));
 			}
 		}
-
+		
 		// Fire HypeDocumentLoad from hypeDocument.functions() if present
 		if (hypeDocument.functions()['HypeDocumentLoad']) {
-			hypeDocument.functions()['HypeDocumentLoad'](hypeDocument, element, event);
+			hypeDocument.functions()['HypeDocumentLoad'](hypeDocument, _element, _event);
 		}
 
 		// Fallback for standalone code not needed when enabling exporter and inclusion in generated script
@@ -110,10 +109,10 @@ def main():
 		# add actions
 		def extra_actions():
 			return [
-				{"label" : "Conditional Behavior", "function" : "HypePowerPack.Conditional_Behavior", "arguments":[{"label":"Expression", "type": "String"}, {"label":"Behavior true", "type": "String"}, {"label":"Behavior false", "type": "String"}]},
-				{"label" : "Set Variable", "function" : "HypePowerPack.Set_Variable", "arguments":[{"label":"Variable", "type": "String"}, {"label":"Expression", "type": "String"}]},
-				{"label" : "Run Function by Selector", "function" : "HypePowerPack.Run_Function_by_Selector", "arguments":[{"label":"Function", "type": "String"}, {"label":"Query Selector", "type": "String"}]},
-				{"label" : "Run JavaScript Expression", "function" : "HypePowerPack.Run_JavaScript_Expression", "arguments":[{"label":"Expression", "type": "String"}]},
+				{"label" : "Conditional Behavior", "function" : "HypePowerPack.conditionalBehavior", "arguments":[{"label":"Expression", "type": "String"}, {"label":"Behavior true", "type": "String"}, {"label":"Behavior false", "type": "String"}]},
+				{"label" : "Set Variable", "function" : "HypePowerPack.setVariable", "arguments":[{"label":"Variable", "type": "String"}, {"label":"Expression", "type": "String"}]},
+				{"label" : "Run Function by Selector", "function" : "HypePowerPack.runFunctionBySelector", "arguments":[{"label":"Function", "type": "String"}, {"label":"Query Selector", "type": "String"}]},
+				{"label" : "Run JavaScript Expression", "function" : "HypePowerPack.runJavaScriptExpression", "arguments":[{"label":"Expression", "type": "String"}]},
 			]
 
 		def save_options():
@@ -126,7 +125,6 @@ def main():
 			"extra_actions" : extra_actions(),
 			"save_options" : save_options(),
 			"min_hype_build_version" : "596",
-			#"max_hype_build_version" : "670",
 		}
 	
 		exit_with_result(options)
@@ -162,8 +160,7 @@ def main():
 					
 		# read and prepare action helper
 		global javascript_for_actions
-		template = string.Template(javascript_for_actions)
-		javascript_for_actions = template.substitute({"hype_id" : hype_id})
+		javascript_for_actions = javascript_for_actions.replace('${hype_id}', hype_id);
 		
 		def modify_generated_script(filepath):
 			# read
